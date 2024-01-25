14d1d8cd0ad161084b89d40558830caa3379364e
ashishbhoure <70540072+ashishbhoure@users.noreply.github.com>
2024-01-25 21:49:43 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (29.13%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,56 +1,93 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int pivot = findPivot(nums);
-        if(pivot == -1){
-            return binarySearch(nums, target,0,nums.length-1);
-        }
-        if(nums[pivot] == target){
-            return pivot;
-        }
+    public int search(int[] arr, int target) {
         
-        if(target >= nums[0]){
-            return binarySearch(nums,target,0,pivot-1);
-        }
-        return binarySearch(nums,target,pivot+1,nums.length-1);
-    }
-    
-    int findPivot(int[] nums ){
-        int start =0;
-        int end = nums.length-1;
+        int l = 0;
+        int r = arr.length-1;
         
-        while(start<= end){
-            int mid = start+(end-start)/2;
-            
-            if(mid<end && nums[mid]>nums[mid+1]){
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(arr[mid]==target){
                 return mid;
             }
             
-            if(mid>start && nums[mid]<nums[mid-1]){
-                return mid-1;
-            }
-            
-            if(nums[mid]<= nums[start]){
-                end = mid-1;
+            if(arr[l] <= arr[mid]){
+                if(arr[l] <= target && target <= arr[mid]){
+                    r = mid-1;
+                }else{
+                    l=mid+1;
+                }
             }else{
-                start = mid+1;
+                if(arr[mid] <= target && target <= arr[r]){
+                    l = mid+1;
+                    
+                }else{
+                    r=mid-1;
+                }
             }
             
-        }
-        return -1;
-    }
-    
-    int binarySearch(int[] arr,int target,int start,int end){
-        while(start<=end){
-            int mid = start+(end-start)/2;
-            
-            if(target<arr[mid]){
-                end= mid-1;
-            }else if(target>arr[mid]){
-                start= mid+1;
-            }else{
-                return mid; 
-            }
         }
         return -1;
     }
 }
+            
+            
+//         int pivot = findPivot(nums);
+//         if(pivot == -1){
+//             return binarySearch(nums, target,0,nums.length-1);
+//         }
+//         if(nums[pivot] == target){
+//             return pivot;
+//         }
+        
+//         if(target >= nums[0]){
+//             return binarySearch(nums,target,0,pivot-1);
+//         }
+//         return binarySearch(nums,target,pivot+1,nums.length-1);
+    // }
+    
+    
+    
+    
+//     int findPivot(int[] nums ){
+//         int start =0;
+//         int end = nums.length-1;
+        
+//         while(start<= end){
+//             int mid = start+(end-start)/2;
+            
+//             if(mid<end && nums[mid]>nums[mid+1]){
+//                 return mid;
+//             }
+            
+//             if(mid>start && nums[mid]<nums[mid-1]){
+//                 return mid-1;
+//             }
+            
+//             if(nums[mid]<= nums[start]){
+//                 end = mid-1;
+//             }else{
+//                 start = mid+1;
+//             }
+            
+//         }
+//         return -1;
+//     }
+    
+//     int binarySearch(int[] arr,int target,int start,int end){
+//         while(start<=end){
+//             int mid = start+(end-start)/2;
+            
+//             if(target<arr[mid]){
+//                 end= mid-1;
+//             }else if(target>arr[mid]){
+//                 start= mid+1;
+//             }else{
+//                 return mid; 
+//             }
+//         }
+//         return -1;
+        
+        
+        
+    // }
+    // }
