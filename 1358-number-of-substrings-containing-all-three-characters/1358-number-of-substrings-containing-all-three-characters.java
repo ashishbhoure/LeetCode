c9b332f1ac26ed5d32a585d327a29cac90016053
Ashish Bhoure <70540072+ashishbhoure@users.noreply.github.com>
2024-03-30 10:52:34 +0530

    Time: 11 ms (89.26%), Space: 44.7 MB (39.15%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i = 0; i < n; i++) {
             last[s.charAt(i) - 'a'] = i;
             ans += Math.min(Math.min(last[0], last[1]), last[2]) + 1;
-            System.out.println(ans);
+            // System.out.println(ans);
         }
         return ans;
     }
