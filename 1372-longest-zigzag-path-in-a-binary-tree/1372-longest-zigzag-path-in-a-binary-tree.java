46a80fc4abf2d045a0451db6ee5d1f589a2d45ae
Ashish Bhoure <70540072+ashishbhoure@users.noreply.github.com>
2024-06-27 20:29:09 +0530

    Time: 5 ms (71.28%), Space: 54.9 MB (47.42%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
@@ -14,30 +14,52 @@
  * }
  */
 class Solution {
-    int maxPath = 0;
     
-    void solve(TreeNode root , int steps, boolean goleft){
+//     method one 
+    
+//     int maxPath = 0;
+    
+//     void solve(TreeNode root , int steps, boolean goleft){
+//         if(root == null){
+//             return;
+//         }    
+        
+//         maxPath = Math.max(maxPath , steps);
+        
+//         if(goleft == true){
+//             solve(root.left,steps+1,false);
+//             solve(root.right,1,true);
+//         }else{
+//             solve(root.right,steps+1,true);
+//             solve(root.left,1,false);
+//         }
+//     }
+    
+    
+    
+//     othe method 
+    
+    int maxPath = 0;
+    void solvePath(TreeNode root,int left,int right){
         if(root == null){
             return;
-        }    
-        
-        maxPath = Math.max(maxPath , steps);
-        
-        if(goleft == true){
-            solve(root.left,steps+1,false);
-            solve(root.right,1,true);
-        }else{
-            solve(root.right,steps+1,true);
-            solve(root.left,1,false);
         }
+        int max = Math.max(left,right);
+        maxPath = Math.max(maxPath , max);
+        
+        solvePath(root.left,right+1,0);
+        solvePath(root.right,0,left+1);
     }
     
     public int longestZigZag(TreeNode root) {
-        if(root == null){
-            return 0;
-        }
-        solve(root.left,1,false);
-        solve(root.right,1,true);
+        // if(root == null){
+            // return 0;
+        // }
+        // solve(root.left,1,false);
+        // solve(root.right,1,true);
+        
+        
+        solvePath(root,0,0);
         
         return maxPath;
     }
