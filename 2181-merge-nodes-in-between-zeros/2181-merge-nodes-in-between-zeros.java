1085755375f2500a97404d3eae3f8588baf87a4e
Ashish Bhoure <70540072+ashishbhoure@users.noreply.github.com>
2024-07-04 11:39:55 +0530

    Time: 12 ms (6.16%), Space: 85.7 MB (10.95%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -10,23 +10,44 @@
  */
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-        ListNode p1 = head.next;
-        ListNode p2 = p1;
+//         ListNode p1 = head.next;
+//         ListNode p2 = p1;
+        
+//         int sum = 0;
+//         while(p2!=null){
+//             while(p2!= null && p2.val != 0){
+//                 sum += p2.val;
+//                 p2 = p2.next;
+//             }
+            
+//             p1.val = sum;
+//             p2 = p2.next;
+//             p1.next = p2;
+//             p1= p1.next;
+//             sum = 0;
+            
+//         }
+//         return head.next;
+        
+        
+        
+//         solution using recursion
+        
+        head = head.next;
+        if(head == null){
+            return head;
+        }
+        ListNode temp = head;
         
         int sum = 0;
-        while(p2!=null){
-            while(p2!= null && p2.val != 0){
-                sum += p2.val;
-                p2 = p2.next;
-            }
-            
-            p1.val = sum;
-            p2 = p2.next;
-            p1.next = p2;
-            p1= p1.next;
-            sum = 0;
-            
+        while(temp != null && temp.val != 0){
+            sum += temp.val;
+            temp = temp.next;
         }
-        return head.next;
+        
+        head.val = sum;
+        head.next = mergeNodes(temp);
+        
+        return head;
     }
 }
